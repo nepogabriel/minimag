fe64660fe69d88ac54f61a0331f5f297b7397f97
Gabriel Ribeiro <gabrielribeiroonline@gmail.com>
2019-12-26 18:03:41 -0200

    Concluido home

---
+++ template_parts/post.php
@@ -0,0 +1,25 @@
+<article>
+    <a href="<?php the_permalink(); ?>">
+        <div class="post_item">
+
+            <!-- número de comentários -->
+            <div class="post_comment_area">
+                <?php comments_number('0', '1', '%'); ?>
+            </div>
+
+            <!-- thumbnail -->
+            <div class="post_info">
+                <?php if(has_post_thumbnail()): ?>
+                    <?php the_post_thumbnail('medium', array('class' => 'post_thumb')); ?>
+                <?php endif; ?>
+            </div>
+            
+            <div class="post_date"> <?php echo get_the_date(); ?> </div>
+
+            <div class="post_title"> <?php the_title(); ?> </div>
+
+            <div class="post_excerpt"> <?php the_excerpt(); ?> </div>
+
+        </div>
+    </a> 
+</article>
